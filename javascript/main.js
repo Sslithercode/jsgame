34b6ce0ee1b73c074a9a881a0a26b82d19eeddd5
Sslithercode <85530935+Sslithercode@users.noreply.github.com>
2022-02-02 07:52:37 -0500

    basics

--- javascript/main.js
+++ javascript/main.js
@@ -2,3 +2,24 @@ canvas = document.getElementById("MainCanvas")
 ctx = win.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
+class Rect{
+    constructor(x,y,width,height){
+        this.x = x
+        this.y = y
+        this.width = width 
+        this.height = height
+    }
+    draw(){
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+r1 = new Rect(0,0,50,50)
+
+
+function update(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    r1.draw()
+}
